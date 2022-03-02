0260f0fd4e9b08723728bd2cbab707be2dcea0b6
tmlsn <theo.malless@gmail.com>
2022-03-02 20:43:47 +0100

    styling reviews + details page css

--- models/models.review.js
+++ models/models.review.js
@@ -10,7 +10,7 @@ const reviewSchema = new mongoose.Schema({
         required: true,
     },
     author: {
-        type: [mongoose.SchemaTypes.ObjectId],
+        type: String,
         required: true,
         ref: 'User',
     }, 
